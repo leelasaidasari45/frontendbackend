7659fa7c0ec0ea8e5f567e938ce65a59bbc3e164
SaiPrasad <leelasaidasari@gmail.com>
2026-04-10 16:02:14 +0530

    fix: correct relative path for Theme import in GlassPanel

--- mobile/src/components/Shared/GlassPanel.js
+++ mobile/src/components/Shared/GlassPanel.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import { Theme } from '../styles/theme';
+import { Theme } from '../../styles/theme';
 
 const GlassPanel = ({ children, style }) => {
   return (
